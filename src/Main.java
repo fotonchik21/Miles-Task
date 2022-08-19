45d251b4a7888d3a5730e21778579432f81ee3a4
Svetlana Latysheva <fotonchik@list.ru>
2022-08-19 20:31:45 +0700

    add

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        int ticketPrice = 100;
+        int bonusCount = 20;
+        int bonus = ticketPrice / bonusCount;
+        System.out.println(bonus);
+    }
+
+}
