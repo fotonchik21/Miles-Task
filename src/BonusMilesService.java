8aa700c55c73a4dea91cb7f00a4c8fe61aab2650
Svetlana Latysheva <fotonchik@list.ru>
2022-08-19 22:29:10 +0700

    add bonismilesservice

---
+++ src/BonusMilesService.java
@@ -0,0 +1,7 @@
+public class BonusMilesService {
+
+    public int calculate(int cost) {
+        int bonusCount = 20;
+        return cost / bonusCount;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,15 @@
 public class Main {
 
     public static void main(String[] args) {
-        int ticketPrice = 100;
-        int bonusCount = 20;
-        int bonus = ticketPrice / bonusCount;
-        System.out.println(bonus);
-    }
+//        int ticketPrice = 100;
+//        int bonusCount = 20;
+//        int bonus = ticketPrice / bonusCount;
+//        System.out.println(bonus);
 
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+
+        int miles = service.calculate(price);
+        System.out.println(miles);
+    }
 }
